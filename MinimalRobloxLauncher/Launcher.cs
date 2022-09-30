def5b50d21c248fc240b62456a27a78688486ab9
yeemi routh <yeemirouth@gmail.com>
2022-09-30 11:33:38 +1000

    Add project files.

---
+++ MinimalRobloxLauncher/Launcher.cs
@@ -0,0 +1,56 @@
+﻿namespace MinimalRobloxLauncher
+{
+    class LauncherArgs // roblox-player:~+launchmode:~+gameinfo:~+launchtime:~+placelauncherurl:~+browsertrackerid:~+robloxLocale:~+gameLocale:~+channel:+LaunchExp:~
+    {
+        public string Gameinfo;
+        public ulong LaunchTime;
+        public string PlaceLauncherUrl;
+        public ulong BrowserTrackerID;
+        public string RobloxLocale;
+        public string GameLocale; // dont need any other arguments
+    }
+
+    class Launcher
+    {
+        public static LauncherArgs ParseArgs(string str)
+        {
+            LauncherArgs returnArgs = new LauncherArgs();
+
+            var args = str.Split('+');
+
+            foreach (string arg in args)
+            {
+                var argTokens = arg.Split(':');
+                switch (argTokens[0])
+                {
+                    case "gameinfo":
+                        returnArgs.Gameinfo = argTokens[1];
+                        break;
+
+                    case "launchtime":
+                        returnArgs.LaunchTime = ulong.Parse(argTokens[1]);
+                        break;
+
+                    case "placelauncherurl":
+                        returnArgs.PlaceLauncherUrl = argTokens[1];
+                        break;
+
+                    case "browsertrackerid":
+                        returnArgs.BrowserTrackerID = ulong.Parse(argTokens[1]);
+                        break;
+
+                    case "robloxLocale":
+                        returnArgs.RobloxLocale = argTokens[1];
+                        break;
+
+                    case "gameLocale":
+                        returnArgs.GameLocale = argTokens[1];
+                        break;
+                }
+            }
+
+            return returnArgs;
+        }
+
+    }
+}
---
+++ MinimalRobloxLauncher/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Principal;
+using System.Web;
+using System.Windows.Forms;
+
+using Microsoft.Win32;
+
+namespace MinimalRobloxLauncher
+{
+    internal class Program
+    {
+        static void ReplaceKey(string app)
+        {
+            RegistryKey key = Registry.ClassesRoot.OpenSubKey($"{app}\\shell\\open\\command", true);
+            key.SetValue(string.Empty,
+                $"\"{AppDomain.CurrentDomain.BaseDirectory + AppDomain.CurrentDomain.FriendlyName}\" %1");
+            key.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0 && new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator))
+            {
+                ReplaceKey("roblox-player");
+                MessageBox.Show("Process replaced", "Success");
+            }
+            else
+            {
+                var la = Launcher.ParseArgs(args[0]);
+
+                // https://clientsettingscdn.roblox.com/v2/client-version/WindowsPlayer
+
+                var robloxVersions = Directory.GetDirectories("C:\\Program Files (x86)\\Roblox\\Versions");
+
+                Process proc = Process.Start(robloxVersions[robloxVersions.Length - 1] + "\\RobloxPlayerBeta.exe",
+                    $"--play -a https://www.roblox.com/Login/Negotiate.ashx -t {la.Gameinfo} " +
+                    $"-j {HttpUtility.UrlDecode(la.PlaceLauncherUrl)} -b {la.BrowserTrackerID} --launchtime={la.LaunchTime} " +
+                    $"--rloc {la.RobloxLocale} --gloc {la.GameLocale}");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
